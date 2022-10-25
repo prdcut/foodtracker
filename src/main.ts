418b758f0d5fcf74547dbd5e936e8b5560a128a0
prdcut <danielfma10@gmail.com>
2022-10-25 17:34:18 +0200

    Add bootstrap and general styles

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "@/styles/_custom.scss";
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
