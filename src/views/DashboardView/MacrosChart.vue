9696571f2be550269a1b5abe88cb43cfe2bb33a3
prdcut <danielfma10@gmail.com>
2022-11-30 13:58:32 +0100

    Create macros chart

---
+++ src/views/DashboardView/MacrosChart.vue
@@ -0,0 +1,87 @@
+<template>
+  <b-card no-body border-variant="white">
+    <vue-apex-charts
+      height="120"
+      :options="macrosChart"
+      :series="series"
+      :eatenMacro="eatenMacro"
+    />
+  </b-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueApexCharts from 'vue-apexcharts';
+
+export default defineComponent({
+  components: {
+    VueApexCharts,
+  },
+  props: {
+    eatenMacro: {
+      type: Array,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      series: [this.eatenMacro],
+      macrosChart: {
+        chart: {
+          type: 'radialBar',
+          sparkline: {
+            enabled: true,
+          },
+          dropShadow: {
+            enabled: true,
+            blur: 3,
+            left: 1,
+            top: 1,
+            opacity: 0.1,
+          },
+        },
+        colors: ['#78a6c8'],
+        plotOptions: {
+          radialBar: {
+            offsetY: -10,
+
+            hollow: {
+              size: '60%',
+            },
+            track: {
+              background: '#e9eef4',
+              strokeWidth: '50%',
+            },
+            dataLabels: {
+              name: {
+                show: false,
+              },
+              value: {
+                offsetY: 8,
+                fontSize: '0.8rem',
+                fontWeight: '600',
+              },
+            },
+          },
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shade: 'dark',
+            type: 'horizontal',
+            shadeIntensity: 0.5,
+            gradientToColors: ['#78a6c8'],
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0, 100],
+          },
+        },
+        stroke: {
+          lineCap: 'round',
+        },
+      },
+    };
+  },
+});
+</script>
---
+++ src/views/DashboardView/RemainingMacros.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-card border-variant="primary" class="h-100" v-if="userProfile">
+    <!-- Title -->
+    <h5 class="mb-2">today's macros</h5>
+    <!-- chart -->
+    <b-row
+      class="d-flex justify-content-between align-items-center"
+      style="height: 80%"
+    >
+      <b-col>
+        <macros-chart :eatenMacro="todayProteinPercentage" />
+        <h6 class="text-center mb-0">
+          <small>
+            Protein {{ todayEatenProtein }} / {{ userProfile.macros.protein }} g
+          </small>
+        </h6>
+      </b-col>
+      <b-col>
+        <macros-chart :eatenMacro="todayCarbsPercentage" />
+        <h6 class="text-center mb-0">
+          <small>
+            Carbs {{ todayEatenCarbs }} / {{ userProfile.macros.carbs }} g
+          </small>
+        </h6>
+      </b-col>
+      <b-col>
+        <macros-chart :eatenMacro="todayFatPercentage" />
+        <h6 class="text-center mb-0">
+          <small>
+            Fat {{ todayEatenFat }} / {{ userProfile.macros.fat }} g
+          </small>
+        </h6>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script lang="ts">
+import DashboardBaseComponent from './DashboardBase';
+import VueApexCharts from 'vue-apexcharts';
+import { Component } from 'vue-property-decorator';
+import MacrosChart from './MacrosChart.vue';
+
+@Component({
+  components: {
+    VueApexCharts,
+    MacrosChart,
+  },
+})
+export default class RemainingMacrosComponent extends DashboardBaseComponent {
+  get todayEatenProtein() {
+    return this.todayFoodDiary
+      .map((x) => x.nutritionalCont?.protein)
+      .reduce((sum, x) => sum! + x!, 0);
+  }
+
+  get todayEatenCarbs() {
+    return this.todayFoodDiary
+      .map((x) => x.nutritionalCont?.carbs)
+      .reduce((sum, x) => sum! + x!, 0);
+  }
+
+  get todayEatenFat() {
+    return this.todayFoodDiary
+      .map((x) => x.nutritionalCont?.fat)
+      .reduce((sum, x) => sum! + x!, 0);
+  }
+
+  get todayProteinPercentage() {
+    const proteinTarget = Number(this.userProfile!.macros?.protein);
+    return [this.calcPercentage(Number(this.todayEatenProtein), proteinTarget)];
+  }
+
+  get todayCarbsPercentage() {
+    const carbsTarget = Number(this.userProfile!.macros?.carbs);
+    return [this.calcPercentage(Number(this.todayEatenCarbs), carbsTarget)];
+  }
+
+  get todayFatPercentage() {
+    const fatTarget = Number(this.userProfile!.macros?.fat);
+    return [this.calcPercentage(Number(this.todayEatenFat), fatTarget)];
+  }
+}
+</script>
